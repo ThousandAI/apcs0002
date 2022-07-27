8857fa4da0ed826aeb1ce2fdafee858e9b2f67ff
ThousandAI <ceo@thousandai.com>
2022-07-27 10:46:05 +0800

    class02.cpp editing

--- class02/src/class02.cpp
+++ class02/src/class02.cpp
@@ -7,10 +7,12 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	// APCS c294
+	/*
 	int a, b, c;
 	cin >> a >> b >> c;
 	int max, mid, min;
@@ -43,5 +45,105 @@ int main() {
 	}else{
 		cout << "Acute";
 	}
+	*/
+
+	// 2維陣列
+	//int scores[3][2] = {{2, 3}, {5, 7}, {7, 8}};
+	/*
+	cout << scores[0][0] << "\n";
+	cout << scores[0][1] << "\n";
+	cout << scores[1][0] << "\n";
+	cout << scores[1][1] << "\n";
+	cout << scores[2][0] << "\n";
+	cout << scores[2][1] << "\n";
+	*/
+
+	/*
+	for(int i = 0; i < 3; ++i){
+		for(int j = 0; j < 2; ++j){
+			if(j == 1)
+				cout << scores[i][j] << "\n";
+			else
+				cout << scores[i][j] << " ";
+		}
+	}
+	*/
+	/*
+	cout << "整個矩陣的記憶體位置: " << scores << " " << &scores[0][0] << endl;
+	cout << "第 0 row 0 column 的記憶體位置: " << scores[0] << " "<< &scores[0][0] << endl;
+	cout << "第 1 row 0 column 的記憶體位置: " << scores[1] << " "<< &scores[1][0] << endl;
+	cout << "第 2 row 0 column 的記憶體位置: " << scores[2] << " "<< &scores[2][0] << endl;
+	*/
+
+	/*
+	for(int i = 0; i < 3; ++i){
+		for(int j = 0; j < 2; ++j){
+			cout << &scores[i][j] << endl;
+		}
+	}
+	*/
+
+	// 2維矩陣 解答 1-1
+	/*
+	int scores[5][5] = {0};
+	int r, c;
+	cin >> r >> c;
+	for(int i = 0; i < r; ++i){
+		for(int j = 0; j < c; ++j){
+			int temp;
+			cin >> temp;
+			scores[i][j] = temp;
+		}
+	}
+
+	for(int i = 0; i < r; ++i){
+		for(int j = 0; j < c; ++j){
+			if(j == c - 1)
+				cout << scores[i][j] << "\n";
+			else
+				cout << scores[i][j] << " ";
+		}
+	}
+	*/
+
+	/*
+	vector< vector<int> > scores;
+	int r, c;
+	cin >> r >> c;
+	for(int i = 0; i < r; ++i){
+		vector<int> vt;
+		for(int j = 0; j < c; ++j){
+			int temp;
+			cin >> temp;
+			vt.push_back(temp);
+		}
+		scores.push_back(vt);
+	}
+
+	for(int i = 0; i < (int)scores.size(); ++i){
+		for(int j = 0; j < (int)scores[0].size(); ++j){
+			if(j == c - 1)
+				cout << scores[i][j] << "\n";
+			else
+				cout << scores[i][j] << " ";
+		}
+	}
+	*/
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
