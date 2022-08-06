70e0f54a9fac3ebac6550d4ee3e55fa8c41e6265
ThousandAI <ceo@thousandai.com>
2022-08-06 14:50:56 +0800

    f605.cpp

---
+++ class03/f605/src/f605.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : f605.cpp
+// Author      : Thousand AI
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	// f605 購買力 2022-01
+	int total = 0;
+	int total_price = 0;
+	int row,target;
+	cin >> row >> target;
+	for(int i = 0; i < row; ++i){
+		int min, max;
+		int a, b, c;
+		cin >> a >> b >> c;
+		if(a >= b){
+			max = a;
+			min = b;
+		}else{
+			min = a;
+			max = b;
+		}
+		if(c > max)
+			max = c;
+		else if(c < min)
+			min = c;
+
+		if(max - min >= target){
+			total += 1;
+			total_price += (a + b + c)/3;
+		}
+	}
+	cout << total << " " << total_price;
+}
