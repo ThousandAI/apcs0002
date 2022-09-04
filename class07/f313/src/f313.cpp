4c067871abc19475e95c5bfb2803a75c642e0764
ThousandAI <ceo@thousandai.com>
2022-09-04 11:01:12 +0800

    f313

--- class07/f313/src/f313.cpp
+++ class07/f313/src/f313.cpp
@@ -9,25 +9,14 @@
 
 #include <iostream>
 using namespace std;
-int a[52][52] = {};
-int b[52][52] = {};
+int a[52][52];
+int b[52][52];
 
-void f(int i, int j, int x, string mode){
-	if(mode == "u")
-		b[i-1][j] += x;
-	else if(mode == "l")
-		b[i][j-1] += x;
-	else if(mode == "d")
-		b[i+1][j] += x;
-	else if(mode == "r")
-		b[i][j+1] += x;
-	b[i][j] -= x;
-}
 
-int main() {
+int main(void){
 	int min = 101;
 	int max = -1;
-	int R, C, k, m;
+	int R, C, k ,m;
 	cin >> R >> C >> k >> m;
 
 	for(int i = 0; i < 52; ++i){
@@ -43,32 +32,38 @@ int main() {
 			b[i][j] = 0;
 		}
 	}
-	for(int z = 0; z < m; ++z){
+
+	for(int l = 0; l < m; ++l){
 		for(int i = 1; i <= R; ++i){
 			for(int j = 1; j <= C; ++j){
 				if(a[i][j] != -1){
 					int x = a[i][j] / k;
-					if(x==0)
+					if(x == 0){
 						continue;
-					if(a[i-1][j] != -1)
-						f(i, j, x, "u");
-					if(a[i][j-1] != -1)
-						f(i, j, x, "l");
-					if(a[i+1][j] != -1)
-						f(i, j, x, "d");
-					if(a[i][j+1] != -1)
-						f(i, j, x, "r");
+					}
+					if(a[i-1][j] != -1){
+						b[i-1][j] += x;
+						b[i][j] -= x;
+					}
+					if(a[i][j-1] != -1){
+						b[i][j-1] += x;
+						b[i][j] -= x;
+					}
+					if(a[i+1][j] != -1){
+						b[i+1][j] += x;
+						b[i][j] -= x;
+					}
+					if(a[i][j+1] != -1){
+						b[i][j+1] += x;
+						b[i][j] -= x;
+					}
 				}
 			}
 		}
+
 		for(int i = 1; i <= R; ++i){
 			for(int j = 1; j <= C; ++j){
-				if(a[i][j] != -1)
-					a[i][j] += b[i][j];
-			}
-		}
-		for(int i = 1; i <= R; ++i){
-			for(int j = 1; j <= C; ++j){
+				a[i][j] += b[i][j];
 				b[i][j] = 0;
 			}
 		}
@@ -85,7 +80,4 @@ int main() {
 
 	cout << min << endl;
 	cout << max;
-
-
-	return 0;
 }
